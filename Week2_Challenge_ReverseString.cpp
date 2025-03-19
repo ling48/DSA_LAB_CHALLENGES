78351cb27000ee1f3fb5b4826c7967fa29522686
ling48 <yassinrahmeto2@gmail.com>
2025-03-19 17:47:19 +0300

    week2_reverse_string

---
+++ Week2_Challenge_ReverseString.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+
+    sol.reverseString(s);
+
+    for (char c : s) {
+        cout << c;
+    }
+    cout << endl;
+
+    return 0;
+}
